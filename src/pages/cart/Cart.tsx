21bc0aa2f006198fc659ce88ae837feac54819c7
Ashvin <ashvinjangid10@gmail.com>
2025-05-01 17:20:23 +0530

    show and style the cart items, add total price and checkout button

--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,5 +1,7 @@
 import { useOutletContext } from "react-router-dom";
 import { Product } from "../shop/useFetchProducts";
+import { Card } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
 function Cart() {
     const [cartItems, updateCartItems] =
@@ -7,7 +9,64 @@ function Cart() {
             [Product[], React.Dispatch<React.SetStateAction<Product[]>>]
         >();
 
-    return <div>This is the cart</div>;
+    if (cartItems.length === 0) {
+        return (
+            <div className="flex h-full items-center justify-center">
+                <span className="text-2xl font-bold">Cart is empty!!</span>
+            </div>
+        );
+    }
+
+    return (
+        <div className="container mx-auto flex flex-col gap-8 p-10">
+            <div className="flex flex-col gap-4">
+                {cartItems.map((item) => (
+                    <Card className="p-4">
+                        <div className="flex gap-4">
+                            <img
+                                src={item.image}
+                                alt={item.title}
+                                className="h-24 w-20"
+                            />
+                            <div className="flex flex-col justify-center">
+                                <span className="text-xl">{item.title}</span>
+                                <span className="text-xl font-bold">
+                                    ${item.price}
+                                </span>
+                            </div>
+                            <div className="ml-auto self-center">
+                                <Button
+                                    className="cursor-pointer"
+                                    variant="destructive"
+                                >
+                                    Remove
+                                </Button>
+                            </div>
+                        </div>
+                    </Card>
+                ))}
+            </div>
+
+            <hr />
+
+            <div>
+                <div className="text-right text-2xl font-bold">
+                    <span>Total: </span>
+                    <span>
+                        ${cartItems.reduce((acc, item) => acc + item.price, 0)}
+                    </span>
+                </div>
+                <div>
+                    <Button
+                        className="mt-4 w-full cursor-pointer"
+                        variant={"default"}
+                    >
+                        Checkout
+                    </Button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Cart;
