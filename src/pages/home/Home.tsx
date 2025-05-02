95e9ee0df8719704851b10f34437abab4c5ec325
Ashvin <ashvinjangid10@gmail.com>
2025-05-02 12:13:14 +0530

    not gonna fix the home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,10 @@
 function Home() {
     return (
         <div>
-            <h1>Welcome to the Home Page</h1>
+            <p className="text-2xl font-bold">Welcome to the Shopping Cart</p>
             <p>This is the home page of our application.</p>
+            <p>The home page sucks</p>
+            <p>This is all you are gonna get. Too lazy to build one</p>
         </div>
     );
 }
